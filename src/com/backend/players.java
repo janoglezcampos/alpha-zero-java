7c419cdb717187be48caabafa0008e5069ff1d40
chopius <75500957+chopius@users.noreply.github.com>
2021-12-06 19:24:01 +0100

    Add files via upload

--- src/com/backend/players.java
+++ src/com/backend/players.java
@@ -4,7 +4,8 @@ package psiC;
 public class players {
 
 private int id;
-private short [] [] Pieces= new short [13] [4];
+private short [] [] Pieces= new short [4] [13];
+//private short [] [] Pieces_temp= new short [4] [13];
 
 public int getId() {
 	return id;
@@ -29,23 +30,38 @@ public players(int id,short [] [] Pieces) {
 	}
 	
 	
-public void insert_Piece(short i, short j ) {
-		Pieces[i] [j]=1;
+public void insert_Piece(short [] e ) {
+		Pieces[e[0]] [e[1]]=1;
 	}
 
 public void delete_Piece(short i,short j ) {
 	
 		Pieces [i] [j]=0;
 }
+
+public void delete_Pieces(short [][] mano ) { //por ahora no se utiliza
+	for (int i=0;i<4; i++) {
+		for (int j=0;j<13; j++) {
+			if(mano[i][j]==1) {
+				Pieces[i][j]=0;
+			}
+		}
+	}
+}
 	
 		
+public void updatePieces() {
+	
+	
+	
+}
 
 public int  get_number_of_Pieces(){  // mas que nada para actualizar en la interfaz
 	
 	int total=0;
 	
-	for (int i=0;i<13; i++) {
-		for (int j=0;j<4; j++) {
+	for (int i=0;i<4; i++) {
+		for (int j=0;j<13; j++) {
 			total+=Pieces[i][j];
 		}
 	}
