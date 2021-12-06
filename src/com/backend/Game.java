26ecfe3647b2b7fdd1e26b90f3a0d5cda4481fb4
janoglezcampos <janoglezcampos@gmail.com>
2021-12-06 11:07:47 +0100

    Porting mtcs code

--- src/com/backend/Game.java
+++ src/com/backend/Game.java
@@ -2,6 +2,10 @@ package com.backend;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.csp_group.utils.GameMatrix;
 
 public class Game {
 
@@ -12,146 +16,112 @@ public class Game {
     See othello/OthelloGame.py for an example implementation.
 	*/
 	
-private ArrayList<players> players;
-//private ArrayList<token> enable_tokens;
-private int number_players;//// la interfaz gr�fica deber� inicilizarlo.
+	public Game() {
+			
+		}
 
 
-public Game() {
-		setPlayer(number_players);
-		
-	}
-
-private void setPlayer(int number ) {  // se llamra desde la interfaz 
-	for (int i=0; i<number; i++) {
-		
-		//players.add(new players(i,dealer()));
-		
-	}	
-}
-
-/*
-private ArrayList<token> dealer() {
-	
-	ArrayList<token> random= new ArrayList<>();
-	enable_tokens= token.getPull();	
-	Collections.shuffle(enable_tokens);
-	for (int i =0; i<14; i++) {
-		random.add(enable_tokens.get(i));	
-	}
-	for (int i =0; i<14; i++) {
-		enable_tokens.remove(i);	
-	}
-	
-	return random;	
-}
-*/
-
-
-
-
-
-
-    public void  getInitBoard ( int self){
-        
-    	
-    	
-    	
-        return;
+    public GameMatrix  getInitBoard ( ){
+        /*
+        Returns:
+            startBoard: a representation of the board (ideally this is the form
+                        that will be the input to your neural network)
+        */
+        return null;
         
      } 
 
-    public void  getBoardSize( int self) {
+    public int  getBoardSize( ) {
         /*
-        Returns:
+        return nulls:
             (x,y): a tuple of board dimensions
         */
-        return;
+        return 0;
     }
 
-    public void  getActionSize( int self) {
+    public int getActionSize( ) {
         /*
-        Returns:
+        return nulls:
             actionSize: number of all possible actions
         */
-        return;
+        return 0;
 	}
 
-    public void  getNextState(int self, int board, int player, int action) {
+    public GameMatrix  getNextState(int board, int player, int action) {
         /*
         Input:
             board: current board
             player: current player (1 or -1)
             action: action taken by current player
-        Returns:
+        return nulls:
             nextBoard: board after applying action
             nextPlayer: player who plays in the next turn (should be -player)
         */
-        return; 
+        return null; 
     }
-    public void  getValidMoves(int self,int  board, int player) {
+    public int[] getValidMoves(GameMatrix board, int player) {
         /*
         Input:
             board: current board
             player: current player
-        Returns:
+        return nulls:
             validMoves: a binary vector of length self.getActionSize(), 1 for
                         moves that are valid from the current board and player,
                         0 for invalid moves
         */
-        return; 
+        return null; 
     }
 
-    public void getGameEnded(int self,int  board, int player) {
+    public int getGameEnded(GameMatrix  board, int player) {
         /*
         Input:
             board: current board
             player: current player (1 or -1)
-        Returns:
+        return nulls:
             r: 0 if game has not ended. 1 if player won, -1 if player lost,
                small non-zero value for draw.
                
         */
-        return;
+        return 0;
     }
-    public void  getCanonicalForm(int self,  int board,  int player){
+    public GameMatrix  getCanonicalForm(GameMatrix next_s,  int player){
         /*
         Input:
             board: current board
             player: current player (1 or -1)
-        Returns:
-            canonicalBoard: returns canonical form of board. The canonical form
+        return nulls:
+            canonicalBoard: return nulls canonical form of board. The canonical form
                             should be independent of player. For e.g. in chess,
                             the canonical form can be chosen to be from the pov
-                            of white. When the player is white, we can return
+                            of white. When the player is white, we can return null
                             board as is. When the player is black, we can invert
-                            the colors and return the board.
+                            the colors and return null the board.
         */
-       return;
+       return null;
     }
 
-    public void getSymmetries(int self,int  board, int pi) {
+    public List<GameMatrix> getSymmetries(GameMatrix board, int pi) {
         /*
         Input:
             board: current board
             pi: policy vector of size self.getActionSize()
-        Returns:
+        return nulls:
             symmForms: a list of [(board,pi)] where each tuple is a symmetrical
                        form of the board and the corresponding pi vector. This
                        is used when training the neural network from examples.
         */
-        return;
+        return null;
     }
 
-    public void  stringRepresentation(int self, int board) {
+    public String  stringRepresentation(GameMatrix board) {
         /*
         Input:
             board: current board
-        Returns:
+        return nulls:
             boardString: a quick conversion of board to a string format.
                          Required by MCTS for hashing.
         */
-        return;
+        return null;
     }
 	
 	
