bfb8bca80b5c7c69d0c71bb934df71e772ae2e52
janoglezcampos <janoglezcampos@gmail.com>
2021-12-07 13:07:51 +0100

    Created main training class

---
+++ src/com/Train.java
@@ -0,0 +1,38 @@
+package com;
+
+import java.util.logging.Logger;
+
+import com.csp_group.utils.Arguments;
+import com.mcts.Coach;
+import com.mcts.Nnet;
+import com.backend.Game;
+
+public class Train {
+	private static Logger log = Logger.getLogger(Train.class.getName());
+	public static void main(String[] args) {
+		Arguments argsAlg = new Arguments();
+	    log.info("Loading game...");
+	    Game g = new Game();
+
+	    log.info("Loading neural net...");
+	    Nnet nnet = new Nnet(g);
+
+	    if(argsAlg.load_model) {
+	        log.info(String.format("Loading checkpoint %s/%s...", argsAlg.load_folder_file[0], argsAlg.load_folder_file[1]));
+	        nnet.load_checkpoint(argsAlg.load_folder_file[0], argsAlg.load_folder_file[1]);
+	    }else {
+	        log.warning("Not loading a checkpoint");
+	    }
+	    log.info("Loading the Coach...");
+	    Coach c = new Coach(g, nnet, argsAlg);
+
+	    if(argsAlg.load_model) {
+	        log.info("Loading 'trainExamples' from file...");
+	        c.loadTrainExamples();
+	    }
+	    log.info("Starting the learning process 🎉");
+	    c.learn();
+
+
+	}
+}
